62601757a37b2a653afba9a5b2285c04a7395f25
Steven-Cadena <steven.cadena.giler@gmail.com>
2022-01-28 09:23:34 +0100

    Add project files.

---
+++ MvcCoreEmpleadosSession/Controllers/EmpleadosController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreEmpleadosSession.Models;
+using MvcCoreEmpleadosSession.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreEmpleadosSession.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private RepositoryEmpleados repo;
+        public EmpleadosController(RepositoryEmpleados repo) 
+        {
+            this.repo = repo;
+        }
+
+        public IActionResult SessionSalarios()
+        {
+            List<Empleado> empleados = this.repo.GetEmpleados();
+            return View(empleados);
+        }
+    }
+}
---
+++ MvcCoreEmpleadosSession/Repositories/RepositoryEmpleados.cs
@@ -0,0 +1,25 @@
+﻿using MvcCoreEmpleadosSession.Data;
+using MvcCoreEmpleadosSession.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreEmpleadosSession.Repositories
+{
+    public class RepositoryEmpleados
+    {
+        private EmpleadosContext context;
+        public RepositoryEmpleados(EmpleadosContext context) 
+        {
+            this.context = context;
+        }
+
+        public List<Empleado> GetEmpleados() 
+        {
+            var consulta = from datos in this.context.Empleados
+                           select datos;
+            return consulta.ToList();
+        }
+    }
+}
